f060177063dda7c2a8305029e5ae08980d3f3e84
lvl7558 <lvl7558@g.rit.edu>
2023-11-22 14:20:55 +0100

    Intial Commit

---
+++ src/main/java/com/example/setup/SetUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetUpApplication.class, args);
+    }
+
+}
